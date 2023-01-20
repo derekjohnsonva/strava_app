b5dafec1281ca25da31d310b7a16179bd8ecfffd
Derek Johnson <dej3tc@virginia.edu>
2023-01-20 12:44:43 -0500

    add days

--- src/routes/log/+page.svelte
+++ src/routes/log/+page.svelte
@@ -1,9 +1,9 @@
-<script>
+<script lang="ts">
 	import { token } from '../../store';
 	import Activity from './Activity.svelte';
+	import type { PageData } from './$types';
 
-	/** @type {import('./$types').PageData} */
-	export let data;
+	export let data: PageData;
 
 	function clearToken() {
 		console.log('Clearing token');
@@ -11,6 +11,31 @@
 		// navigate back to home page
 		location.href = '/';
 	}
+
+	// Map to store days of the week and if they have been rendered
+	const daysRendered = new Map();
+	const daysOfTheWeek = [
+		'Sunday',
+		'Monday',
+		'Tuesday',
+		'Wednesday',
+		'Thursday',
+		'Friday',
+		'Saturday'
+	];
+	daysOfTheWeek.forEach((day) => {
+		daysRendered.set(day, false);
+	});
+
+	const getDayOfWeek = (date: Date) => {
+		const day = date.getDay();
+		return daysOfTheWeek[day];
+	};
+	const getAndSetDayOfWeek = (date: Date) => {
+		const day = date.getDay();
+		daysRendered.set(daysOfTheWeek[day], true);
+		return daysOfTheWeek[day];
+	};
 </script>
 
 <button
@@ -21,5 +46,10 @@
 </button>
 
 {#each [...data.props.activities].reverse() as activity}
+	{#if !daysRendered.get(getDayOfWeek(new Date(activity.start_date)))}
+		<h3>
+			{getAndSetDayOfWeek(new Date(activity.start_date))}
+		</h3>
+	{/if}
 	<Activity {activity} />
 {/each}
--- src/routes/log/Activity.svelte
+++ src/routes/log/Activity.svelte
@@ -13,28 +13,20 @@
 		const seconds = Math.floor((fractionalMinutes - minutes) * 60).toFixed(0);
 		return `${minutes}:${seconds}`;
 	};
-
-	const getDayOfWeek = (date: Date) => {
-		const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-		const day = date.getDay();
-		return days[day];
-	};
 </script>
 
-<h3>
-	{getDayOfWeek(new Date(activity.start_date))}
-</h3>
 <div class="flex flex-row p-0 gap-5 ">
-	<h4 class="w-36">
+	<!-- TODO: Find a more responsive way to keep things in line -->
+	<h4 class="w-36 flex-none">
 		{activity.name}
 	</h4>
-	<h4 class="w-36">
+	<h4 class="w-36 flex-none">
 		{metersToMiles(activity.distance)} miles
 	</h4>
-	<h4 class="w-16">
+	<h4 class="w-16 flex-none">
 		{metersPerSecToMinPerMile(activity.average_speed)}
 	</h4>
-	<body class="w-96">
+	<body class="w-96 flex-none">
 		{activity.description || ''}
 	</body>
 </div>
