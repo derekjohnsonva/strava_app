5a34abf8c3cc5f383922318d98da421745bd06dc
Derek Johnson <dej3tc@virginia.edu>
2023-02-16 12:05:49 -0500

    flex wrap

--- src/routes/log/Activity.svelte
+++ src/routes/log/Activity.svelte
@@ -5,18 +5,26 @@
 </script>
 
 <!-- Add a margin to the div but not its children -->
-<div class="flex flex-row gap-5 text-grey-light m-0">
+<div class="flex flex-wrap text-grey-light text-lg">
 	<!-- TODO: Find a more responsive way to keep things in line -->
-	<h4 class="w-36 flex-none">
-		{activity.name}
-	</h4>
-	<h4 class="w-36 flex-none">
-		{metersToMiles(activity.distance)} miles
-	</h4>
-	<h4 class="w-16 flex-none">
-		{metersPerSecToMinPerMile(activity.average_speed)}
-	</h4>
-	<body class="w-96 flex-none">
-		{activity.description || ''}
-	</body>
+	<div class="w-36">
+		<h4>
+			{activity.name}
+		</h4>
+	</div>
+	<div class="w-36">
+		<h4>
+			{metersToMiles(activity.distance)} miles
+		</h4>
+	</div>
+	<div class="w-16">
+		<h4>
+			{metersPerSecToMinPerMile(activity.average_speed)}
+		</h4>
+	</div>
+	<div class="w-96 text-base">
+		<body>
+			{activity.description || ''}
+		</body>
+	</div>
 </div>
